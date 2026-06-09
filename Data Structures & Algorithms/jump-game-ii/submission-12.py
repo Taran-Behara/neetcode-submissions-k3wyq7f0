ffda01a8641a2bf45b7cb19d5d226b5b879994fd
Taran-Behara <137260274+Taran-Behara@users.noreply.github.com>
2026-06-09 14:13:03 -0700

    Add: jump-game-ii - submission-12

---
+++ Data Structures & Algorithms/jump-game-ii/submission-12.py
@@ -0,0 +1,18 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        jumps = 0
+        l = 0
+        r = 0
+
+        while r < len(nums) - 1:
+            max_in_range = 0
+            for i in range(l, r + 1):
+                max_in_range = max(nums[i], max_in_range)
+            
+            l += 1
+            r += max_in_range
+            jumps += 1
+        
+        return jumps
+
+            
